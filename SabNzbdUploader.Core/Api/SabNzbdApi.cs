1d699ce6ea599168ed3558181d28ada5716ae507
Henrik Grotle <henrik@pswin.com>
2012-08-14 23:05:41 +0200

    Implemented uploading file to sabnzbd

--- SabNzbdUploader.Core/Api/SabNzbdApi.cs
+++ SabNzbdUploader.Core/Api/SabNzbdApi.cs
@@ -20,30 +20,53 @@ namespace Arasoft.SabNzdbUploader.Core.Api
             }
         };
 
+        public Func<string, string, string> HttpSendFile = (url, file) =>
+        {
+            using (var httpclient = new System.Net.WebClient())
+            {
+                return System.Text.Encoding.ASCII.GetString(httpclient.UploadFile(url, file));
+            }
+        };
+
         public List<string> GetCategories()
         {
             var d = JObject.Parse(HttpGet(BuildUrl("api?mode=get_cats&output=json")));
-            
             dynamic data = d.AsDynamic();
-
-
             List<string> categories = new List<string>();
-
             foreach (var category in data.categories)
             {
                 categories.Add(category);
             }
-
             return categories;
+        }
+
+        public void UploadFile(System.IO.FileInfo file, string category)
+        {
+            var resource = BuildUrl(String.Format("api?mode=addfile&name={0}&cat={1}", file.Name, category));
+            HttpSendFile(resource, file.FullName);
         }
 
         private string BuildUrl(string resource)
         {
-            var rooturl = ConfigurationManager.AppSettings["sabRootUrl"];
-            var apikey = ConfigurationManager.AppSettings["sabApiKey"];
-
-            return String.Format("{0}{1}&apikey={2}", rooturl, resource, apikey);
+            return String.Format("{0}{1}&apikey={2}", SabRootUrl, resource, SabApiKey);
         }
+
+        private string SabRootUrl
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["sabRootUrl"];
+            }
+        }
+
+        private string SabApiKey
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["sabApiKey"];
+            }
+        }
+
 
     }
 }
--- SabNzbdUploader.Tests/ApiTests.cs
+++ SabNzbdUploader.Tests/ApiTests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using NUnit.Framework;
 using Arasoft.SabNzdbUploader.Core.Api;
+using System.IO;
 
 namespace SabNzbdUploader.Tests
 {
@@ -31,6 +32,16 @@ namespace SabNzbdUploader.Tests
             Assert.AreEqual("http://localhost:8080/api?mode=get_cats&output=json&apikey=theapikey", called_url);
         }
 
+        [Test]
+        public void Upload_file()
+        {
+            api_returns = "";
+            api.UploadFile(new FileInfo(@"c:\folder\the_file"), "the_category");
+
+            Assert.AreEqual("http://localhost:8080/api?mode=addfile&name=the_file&cat=the_category&apikey=theapikey", called_url);
+            Assert.AreEqual(@"c:\folder\the_file", uploaded_file);
+        }
+
         [SetUp]
         public void Setup()
         {
@@ -41,10 +52,18 @@ namespace SabNzbdUploader.Tests
                 called_url = url;
                 return api_returns;
             };
+
+            api.HttpSendFile = (url, file) =>
+            {
+                called_url = url;
+                uploaded_file = file;
+                return api_returns;
+            };
         }
 
         string called_url;
         string api_returns;
         SabNzbdApi api;
+        string uploaded_file;
     }
 }
